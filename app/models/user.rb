93baa539f7a3bbcbdf6ef8d4ed36d35d0d65136c
Jason Truluck <jason.truluck@gmail.com>
2014-03-13 16:40:07 -0400

    Update the user model
    Includes the bcrypt extension to use for setting the encrypted password
    of the user.
    
    Added getters and setters for password and password confirmation.
    
    The setter and getter for password encrypts and checks the password of
    the user provided respectively using bcrypt. Also added validations so
    these do not have to be done by comparison in the controller.
    
    This could be refined a bit more but that should be done with testing
    :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :galleries, foreign_key: :artist_id
   has_many :paintings, through: :galleries
   has_many :comments
+
+  validates_presence_of :email, :encrypted_password
+  validates_uniqueness_of :email
+
+  def password
+    @password ||= Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password Password.create(encrypted_password)
+    self.encrypted_password = @password
+  end
 
   def name?
     first_name && last_name
